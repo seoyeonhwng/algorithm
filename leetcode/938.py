e7ddd297536fdb1edbbdc2a0aa11418d8fb1c568
seoyeonhwng <seoyeon@nowbusking.com>
2020-10-22 19:24:02 +0900

    1022

---
+++ leetcode/938.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+        stack = [root]
+        sum = 0
+        
+        while stack:
+            node = stack.pop(0)
+            if node:
+                if node.val > L:
+                    stack.append(node.left)
+                if node.val < R:
+                    stack.append(node.right)
+                if L <= node.val <= R:
+                    sum += node.val
+                
+        return sum
